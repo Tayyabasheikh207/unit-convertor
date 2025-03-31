f3f8379a88068dc5ba1adf6eff60013e252afddc
Tayyaba Sheikh 👑 <nomiasma289@gmail.com>
2025-03-31 22:26:47 +0100

    unit convertor.py

--- unit convertor.py
+++ unit convertor.py
@@ -71,7 +71,7 @@ if conversion_type == "Length":
         to_unit = st.selectbox("To", ["Meters", "Kilograms", "Centimeters", "Milimeters", "Miles", "Feet", "Yards", "Inches"])
 elif conversion_type == "Weight":
     with col1:
-        to_unit = st.selectbox("To", "Kilometer", "Grams", "Pounds", "Ounces", "   Miligrams"])
+        to_unit = st.selectbox("To", ["Kilometer", "Grams", "Pounds", "Ounces", "   Miligrams"])
     with col2:
         to_unit = st.selectbox("To", ["Kilometer", "Grams", "Pounds", "Ounces", "Miligrams"])
 elif conversion_type == "Temperature":
